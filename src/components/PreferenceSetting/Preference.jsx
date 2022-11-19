276ab0465a6d637ba6fabe2d085a4a3b3539c44a
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-11-19 13:37:25 +0100

    build

---
+++ src/components/PreferenceSetting/Preference.jsx
@@ -0,0 +1,13 @@
+import PrefSetHNN from "./PreferenceSettingHeadnNav";
+import PrefSetForm from "./PreferenceSettingForm";
+
+function Preference() {
+	return (
+		<>
+			<PrefSetHNN />
+			<PrefSetForm />
+		</>
+	);
+}
+
+export default Preference;
