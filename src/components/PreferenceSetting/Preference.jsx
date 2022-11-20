da9daa638bea93c9e75febc250894fa2415698c2
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-11-20 21:55:15 +0100

    style

--- src/components/PreferenceSetting/Preference.jsx
+++ src/components/PreferenceSetting/Preference.jsx
@@ -1,12 +1,12 @@
 import PrefSetHNN from "./PreferenceSettingHeadnNav";
 import PrefSetForm from "./PreferenceSettingForm";
-
+import style from "./Preference.module.css";
 function Preference() {
 	return (
-		<>
+		<div className={style.preference}>
 			<PrefSetHNN />
 			<PrefSetForm />
-		</>
+		</div>
 	);
 }
 
--- src/components/PreferenceSetting/PreferenceSettingHeadnNav.jsx
+++ src/components/PreferenceSetting/PreferenceSettingHeadnNav.jsx
@@ -4,34 +4,72 @@ import arrowright from "../../images/arrow-right.svg";
 import setting from "../../images/setting-2.svg";
 import logout from "../../images/logout.svg";
 import style from "./Preferenceside.module.css";
+import menu from "../../images/menu.svg";
+import { useState } from "react";
+//import { Link } from "react-router-dom";
 
 function PrefSetHNN() {
+	const [openMenu, setOpenMenu] = useState(false);
+	const toggleMenu = () => {
+		setOpenMenu(!openMenu);
+	};
+
 	return (
-		<>
+		<div className={style.prefside}>
+			<img
+				src={menu}
+				onClick={toggleMenu}
+				className={style.prefsidemenu}
+				alt=""
+			/>
+
 			<img src={preflogo} alt="" />
+			{openMenu ? (
+				<div className={style.prefsidemob}>
+					<a href="/" className={style.prefsiderow}>
+						<img src={setting} alt="" />
+						<p className={style.prefsidetext}>Setting</p>
+						<img src={arrowright} alt="" />
+					</a>
+					<a href="/" className={style.prefsiderow}>
+						<p className={style.prefsidetext}>Preference</p>
+					</a>
+					<div className={style.prefsiderow}>
+						<p className={style.prefsidetext}>My Profile</p>
+					</div>
+					<a href="/" className={style.prefsiderow}>
+						<img src={logout} alt="" />
+						<p className={style.prefsidetext} id={style.logouttxt}>
+							Logout
+						</p>
+					</a>
+				</div>
+			) : null}
 
 			<div className={style.prefsidemain}>
-				<div className={style.prefsiderow}>
+				<a href="/" className={style.prefsiderow}>
 					<img src={arrowleft} alt="" />
 					<p className={style.prefsidetext}>Back</p>
-				</div>
-				<div className={style.prefsiderow}>
+				</a>
+				<a href="/" className={style.prefsiderow}>
 					<img src={setting} alt="" />
 					<p className={style.prefsidetext}>Setting</p>
 					<img src={arrowright} alt="" />
-				</div>
-				<div className={style.prefsiderow}>
+				</a>
+				<a href="/" className={style.prefsiderow}>
 					<p className={style.prefsidetext}>Preference</p>
-				</div>
+				</a>
 				<div className={style.prefsiderow}>
 					<p className={style.prefsidetext}>My Profile</p>
 				</div>
-				<div className={style.prefsiderow}>
+				<a href="/" className={style.prefsiderow}>
 					<img src={logout} alt="" />
-					<p className={style.prefsidetext}>Logout</p>
-				</div>
+					<p className={style.prefsidetext} id={style.logouttxt}>
+						Logout
+					</p>
+				</a>
 			</div>
-		</>
+		</div>
 	);
 }
 
